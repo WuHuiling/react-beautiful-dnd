fd4552d7c54f3cc913c38184355f7ed62b28c3bc
Huiling Wu <huiling@teambition.com>
2018-11-15 12:04:27 +0800

    draggables can only be dragged among the same type

--- src/state/get-drag-impact/in-foreign-list.js
+++ src/state/get-drag-impact/in-foreign-list.js
@@ -15,6 +15,7 @@ import getDisplacement from '../get-displacement';
 import getDisplacementMap from '../get-displacement-map';
 import isUserMovingForward from '../user-direction/is-user-moving-forward';
 import getDisplacedBy from '../get-displaced-by';
+import isSameType from './is-same-type';
 
 type Args = {|
   pageBorderBoxCenterWithDroppableScrollChange: Position,
@@ -52,9 +53,11 @@ export default ({
   const targetCenter: number = currentCenter[axis.line];
   const displacement: number = displacedBy.value;
 
-  const displaced: Displacement[] = insideDestination
+  const dimensionIndexes = [];
+
+  const dimensions: DraggableDimension[] = insideDestination // prettier-ignore
     .filter(
-      (child: DraggableDimension): boolean => {
+      (child: DraggableDimension, index: number): boolean => {
         const borderBox: Rect = child.page.borderBox;
         const start: number = borderBox[axis.start];
         const end: number = borderBox[axis.end];
@@ -65,6 +68,9 @@ export default ({
         // When in foreign list, can only displace forwards
         // Moving forward will decrease the amount of things needed to be displaced
         if (isMovingForward) {
+          if (targetCenter <= start + displacement) {
+            dimensionIndexes.push(index);
+          }
           return targetCenter <= start + displacement;
         }
 
@@ -75,18 +81,47 @@ export default ({
         // - move backwards in the first position
         // - enter into a foreign list moving backwards
 
+        if (targetCenter < end) {
+          dimensionIndexes.push(index);
+        }
         return targetCenter < end;
       },
-    )
-    .map(
-      (dimension: DraggableDimension): Displacement =>
-        getDisplacement({
-          draggable: dimension,
-          destination,
-          previousImpact,
-          viewport: viewport.frame,
-        }),
     );
+
+  function getSortableDimensions(): DraggableDimension[] {
+    const isEndOfTheList = dimensions.length === insideDestination.length;
+
+    if (!dimensions.length || isEndOfTheList) {
+      return dimensions;
+    }
+
+    const firstDimension = dimensions[0];
+    const firstDimensionIsSortable = isSameType(firstDimension, draggable);
+
+    const previousDimensionIndex = dimensionIndexes[0] - 1;
+    const previousDimension = insideDestination[previousDimensionIndex] || null;
+    const previousDimensionIsSortable = isSameType(
+      previousDimension,
+      draggable,
+    );
+
+    if (firstDimensionIsSortable || previousDimensionIsSortable) {
+      return dimensions;
+    }
+
+    return [];
+  }
+
+  const sortableDimensions: DraggableDimension[] = getSortableDimensions();
+  const displaced: Displacement[] = sortableDimensions.map(
+    (dimension: DraggableDimension): Displacement =>
+      getDisplacement({
+        draggable: dimension,
+        destination,
+        previousImpact,
+        viewport: viewport.frame,
+      }),
+  );
 
   const newIndex: number = insideDestination.length - displaced.length;
 
